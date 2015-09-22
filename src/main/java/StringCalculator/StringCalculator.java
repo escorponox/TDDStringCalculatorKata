2cd0d118740195a2d3a1768c38ccff19eb29e7bf
Carlos Coves <carlos.coves.prieto@gmail.com>
2015-09-22 18:10:55 +0200

    givenOneNumberItReturnsTheNumber

--- src/main/java/StringCalculator/StringCalculator.java
+++ src/main/java/StringCalculator/StringCalculator.java
@@ -3,6 +3,8 @@ package StringCalculator;
 public class StringCalculator {
 
     public int add(String s) {
+        if (s.length() > 0)
+            return Integer.valueOf(s);
         return 0;
     }
 }
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -2,16 +2,29 @@ package calculator;
 
 import StringCalculator.StringCalculator;
 import junit.framework.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import static junit.framework.Assert.*;
 
 public class StringCalculatorTest {
 
-    @Test
-    public void givenEmptyStringItReturnsZero() throws Exception {
+    StringCalculator stringCalculator;
 
-        StringCalculator stringCalculator = new StringCalculator();
-
-        Assert.assertEquals(0,stringCalculator.add(""));
+    @Before
+    public void setUp() {
+        stringCalculator = new StringCalculator();
     }
 
+    @Test
+    public void givenEmptyStringItReturnsZero() throws Exception {
+        assertEquals(0, stringCalculator.add(""));
+    }
+
+    @Test
+    public void givenOneNumberItReturnsTheNumber() throws Exception {
+        assertEquals(1, stringCalculator.add("1"));
+        assertEquals(2, stringCalculator.add("2"));
+        assertEquals(3, stringCalculator.add("3"));
+    }
 }
